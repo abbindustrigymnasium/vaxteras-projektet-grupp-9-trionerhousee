8997a333a9bfe2d2c5e7a06d9b4881412fe6f3a3
abbsimmei <simon.meier@abbgymnasiet.se>
2023-01-24 16:04:54 +0100

    Ladde till bild

--- src/pages/IndexPage.vue
+++ src/pages/IndexPage.vue
@@ -2,9 +2,19 @@
   <q-page class="row bg-grey">
     <SideBar></SideBar>
     <div class="BoxWithBoxIn bg-red column flex justify-center items-center justify-around">
-      <div class="Abox bg-pink"></div>
-      <div class="Abox bg-yellow"></div>
-      <div class="Abox bg-pink"></div>
+      <div class="Abox bg-pink flex row justify-end">
+
+        <img class="picture q-ma-xl" src="../../public/icons/bild_på_växthus_här.png">
+
+        <q-slider class="" v-model="value" :min="10" :max="50" :step="1" label color="light-green" />
+
+      </div>
+      <div class="Abox bg-yellow">
+
+      </div>
+      <div class="Abox bg-pink">
+
+      </div>
     </div>
 
 
@@ -18,6 +28,7 @@ import { db } from 'src/boot/firebase'
 import { useDatabaseList } from 'vuefire'
 import { ref as dbref } from 'firebase/database'
 
+const value = ref(0)
 
 </script>
 
@@ -33,5 +44,10 @@ import { ref as dbref } from 'firebase/database'
 .Abox {
   width: 130vh;
   height: 60vh;
+}
+
+.picture {
+  width: 60vh;
+  height: 40vh;
 }
 </style>
