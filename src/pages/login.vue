32196691787908623e270d81d5ac5ac83510dfd4
sebastian200 <kotte.alin@gmail.com>
2023-02-02 08:22:39 +0100

    mannen

---
+++ src/pages/login.vue
@@ -0,0 +1,33 @@
+
+<template>
+  <div>
+    <h1>login</h1>
+    <form @submit.prevent="handlesubmit">
+      <input type="email" v-model="email">
+      <input type="password" v-model="password">
+      <button type="submit" @click="googleSignIn">login</button>
+    </form>
+  </div>
+</template>
+
+<script>
+  import firebase from "firebase";
+
+ export default {
+  data() {
+    return {
+      email: "",
+      password: "",
+
+    };
+  },
+  methods: {
+    googleSignIn: function() {
+      const provider = firebase.auth.GoogleAuthProvider();
+      firebase.auth().signInWithPopup(provider).then (() => {
+        alert("Signed in");
+      })
+    }
+  }
+ }
+</script>
