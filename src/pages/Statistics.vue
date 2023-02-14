231c44fba14d5e849b776addd57e9c2e1f4cfc05
sebastian200 <kotte.alin@gmail.com>
2023-02-14 09:03:01 +0100

    background

--- src/pages/Statistics.vue
+++ src/pages/Statistics.vue
@@ -41,6 +41,9 @@ const liveData = useDatabaseObject(dbref(db, 'LiveData'))
 </script>
 
 <style>
+.background {
+  background-image: url("../../public/icons/Wall_of_Ivy_Leaves_1.jpg");
+}
 .BoxWithBoxIn {
   margin-left: 8vh;
   margin-top: 30px;
@@ -54,6 +57,4 @@ const liveData = useDatabaseObject(dbref(db, 'LiveData'))
   height: 60vh;
   border-radius: 20px 20px 20px 20px;
 }
-
-
 </style>
