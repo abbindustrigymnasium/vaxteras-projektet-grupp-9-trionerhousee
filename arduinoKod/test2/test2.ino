e31feef154e0b858ae37555e466fd4dae52abee3
abb22axerox <axel.roxenborg@abbgymnasiet.se>
2023-02-20 16:02:18 +0100

    remove

--- arduinoKod/test2/test2.ino
+++ arduinoKod/test2/test2.ino
@@ -135,6 +135,7 @@ void checkAll()
     {
       if (hours == 15)
       {
+
         if (minutes == 18)
         {
           Serial.println("pump-ON-Oken");
@@ -177,7 +178,7 @@ void checkAll()
     }
     else if (regnskog == true)
     {
-      if (liveLuft < 95) {
+
         Serial.println(minutes);
         if (minutes == 0 || minutes == 5 || minutes == 10 || minutes == 15 || minutes == 20 || minutes == 25 || minutes == 30 || minutes == 35 || minutes == 40 || minutes == 45 || minutes == 50 || minutes == 55)
         {
@@ -192,13 +193,6 @@ void checkAll()
           analogWrite(pumpSpeed, 0);
           Serial.println(minutes);
         }
-      }
-      else
-      {
-        Serial.println("pump-OFF 1");
-        analogWrite(pumpSpeed, 0);
-        Serial.println(minutes);
-      }
     }
   }
   else
